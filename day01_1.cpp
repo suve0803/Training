c87d8b2b4638fe3d30f2cbbb5d33926e2ea2703e
suvetha <113636722+suve0803@users.noreply.github.com>
2025-05-29 09:16:38 +0530

    Update day01_1.cpp

--- day01_1.cpp
+++ day01_1.cpp
@@ -1,40 +1,56 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-// Base class: Animal
-class Animal {
+// Base class: Vehicle
+class Vehicle {
+protected:
+    string make;
+    string model;
+
 public:
-    void make_sound() {
-        cout << "Animal makes a sound" << endl;
+    // Constructor
+    Vehicle(string make, string model) : make(make), model(model) {}
+
+    // Methods
+    void start() {
+        cout << "Starting the " << make << " " << model << endl;
+    }
+
+    void stop() {
+        cout << "Stopping the " << make << " " << model << endl;
     }
 };
 
-// Derived class: Dog
-class Dog : public Animal {
+// Derived class: Car
+class Car : public Vehicle {
+private:
+    int numberOfDoors;
+
 public:
-    void make_sound() {
-        cout << "Dog barks" << endl;
+    // Constructor
+    Car(string make, string model, int doors) : Vehicle(make, model), numberOfDoors(doors) {}
+
+    // Method specific to Car
+    void honk() {
+        cout << "Honking the horn of the " << make << " " << model << endl;
     }
 };
 
-// Derived class: Cat
-class Cat : public Animal {
-public:
-    void make_sound() {
-        cout << "Cat meows" << endl;
-    }
-};
-
+// Main class to demonstrate functionality
 int main() {
-    // Creating objects of derived classes
-    Dog dog;
-    Cat cat;
+    // Creating a generic vehicle
+    Vehicle genericVehicle("Generic", "Vehicle");
+    genericVehicle.start();
+    genericVehicle.stop();
 
-    // Calling overridden methods
-    cout << "Dog's sound: ";
-    dog.make_sound();
+    cout << endl;
 
-    cout << "Cat's sound: ";
-    cat.make_sound();
+    // Creating a specific car
+    Car car("Toyota", "Camry", 4);
+    car.start();
+    car.honk();
+    car.stop();
 
     return 0;
+}
